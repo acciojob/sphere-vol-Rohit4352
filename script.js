8bac90ffd5e4a5b106cadec0323ab2f9b7029ff0
Rohit Gupta <65344659+Rohit4352@users.noreply.github.com>
2023-06-20 17:30:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius = document.getElementById("radius").value;
+	radius = Math.abs(radius);
+	var volume = (4/3)*Math.PI*Math.pow(radius,3);
+	volume = volume.toFixed(4);
+	//alert(volume);
+	
+	document.getElementById("volume").value = volume;
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
